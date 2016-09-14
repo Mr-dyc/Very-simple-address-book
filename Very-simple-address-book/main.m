52872e5f21cec06475d94abd89cddbad61f6373a
丁云川 <753629152@qq.com>
2016-09-14 10:25:38 +0800

    Initial Commit

---
+++ Very-simple-address-book/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Very-simple-address-book
+//
+//  Created by 丁云川 on 16/9/14.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
